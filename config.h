2f0c4b3df166f320a00555c9fd8e9aec6bf88852
Petelax <peterxiong101@gmail.com>
2022-07-10 23:27:51 -0400

    idk

--- config.h
+++ config.h
@@ -122,7 +122,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 float alpha = 0.95;
